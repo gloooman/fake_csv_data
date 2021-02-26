687a847766e4777e974f227fdbb6852b5d678b81
Ruslan Manzulia <https://Ruslan_Manzulia@bitbucket.org>
2021-02-26 16:45:04 +0200

    del broker

--- csv_creator/settings.py
+++ csv_creator/settings.py
@@ -144,19 +144,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
-BROKER_URL = os.environ.get("REDISCLOUD_URL", "django://")
-BROKER_POOL_LIMIT = 1
-BROKER_CONNECTION_MAX_RETRIES = None
-
-CELERY_TASK_SERIALIZER = "json"
-CELERY_ACCEPT_CONTENT = ["json", "msgpack"]
-CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
-
-if BROKER_URL == "django://":
-    INSTALLED_APPS += ("kombu.transport.django",)
-
-BROKER_TRANSPORT_OPTIONS = {
-    "max_connections": 2,
-}
-BROKER_POOL_LIMIT = None
